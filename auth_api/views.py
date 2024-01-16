f8dd696258a87f55cb1c18d644bfcd85fb8d8542
Daniel Lucio <danielluma242004@hotmail.com>
2024-01-15 19:24:02 -0600

    Fix

--- auth_api/views.py
+++ auth_api/views.py
@@ -20,24 +20,24 @@ def register(request):
         try:
             validate_email(email)
         except ValidationError:
-            return JsonResponse({'message': 'Invalid email address', 'errorType': 1}, status=409)
+            return JsonResponse({'message': '* Invalid email address', 'errorType': 1}, status=409)
 
         if not (1 <= len(username) <= 30) or not any(char.isupper() for char in username) or not any(char.isdigit() for char in username):
-            return JsonResponse({'message': 'Username must be between 1 and 15 characters and contain at least one uppercase letter and one digit', 'errorType': 2}, status=409)
+            return JsonResponse({'message': '* Username must be between 1 and 15 characters and contain at least one uppercase letter and one digit', 'errorType': 2}, status=409)
 
         if not (8 <= len(password)) or not any(char.isupper() for char in password) or not any(char.isdigit() for char in password):
-            return JsonResponse({'message': 'Username must be at least 8 characters and contain at least one uppercase letter and one digit', 'errorType': 3}, status=409)
+            return JsonResponse({'message': '* Password must be at least 8 characters and contain at least one uppercase letter and one digit', 'errorType': 3}, status=409)
 
         if not User.objects.filter(username=username).exists():
             if not User.objects.filter(email=email).exists():
                 user = User.objects.create_user(username=username, email=email, password=make_password(password), first_name=first_name, last_name=last_name)
-                return JsonResponse({'message': 'Registration successful', 'user': username})
+                return JsonResponse({'message': '* Registration successful', 'user': username})
             else:
-                return JsonResponse({'message': 'Email already taken', 'errorType': 4}, status=409)
+                return JsonResponse({'message': '* Email already taken', 'errorType': 4}, status=409)
         elif not User.objects.filter(email=email).exists():
-            return JsonResponse({'message': 'Username already taken', 'errorType': 5}, status=409)
+            return JsonResponse({'message': '* Username already taken', 'errorType': 5}, status=409)
         else:
-            return JsonResponse({'message': 'Username and email already taken', 'errorType': 6}, status=409)
+            return JsonResponse({'message': '* Username and email already taken', 'errorType': 6}, status=409)
     else:
         return JsonResponse({'message': 'Invalid request method'}, status=400)
 
--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -102,7 +102,7 @@ export function Navigation() {
               className="bg-blue-700 p-2 rounded-3xl"
               onClick={handleLogout}
             >
-              Logout
+              Sign Out
             </button>
           </div>
         ) : (
@@ -112,7 +112,7 @@ export function Navigation() {
             className="m-3 text-white bg-blue-700 hover:bg-blue-800 active:bg-blue-700 active:ring-2 active:ring-blue-700  font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
             type="button"
           >
-            Log In
+            Sign In
           </button>
         )}
         {isModalOpen && (
@@ -135,7 +135,7 @@ export function Navigation() {
                   </svg>
                 </button>
               </div>
-              <h2 className="text-2xl font-bold mb-6">Log In</h2>
+              <h2 className="text-2xl font-bold mb-6 ml-3">Sign In</h2>
               <input
                 type="text"
                 placeholder="Username"
@@ -154,7 +154,7 @@ export function Navigation() {
                 <p className="text-red-500 mx-3 ">{errorMessage}</p>
               )}
               <p className="flex flex-row p-3 mb-4">
-                New to Weather App?{" "}
+                New to Authentication App?{" "}
                 <a
                   onClick={() => {
                     toggleModal();
@@ -171,7 +171,7 @@ export function Navigation() {
                 }}
                 className=" bg-blue-700 hover:bg-blue-800 active:bg-blue-700 active:ring-2 active:ring-blue-700 text-white font-bold py-2 px-4 rounded-full"
               >
-                Log In
+                Continue
               </button>
             </div>
           </div>
@@ -179,7 +179,7 @@ export function Navigation() {
         ;
         {isModalRegisterOpen && (
           <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white p-6 rounded-lg shadow-md flex flex-col">
+            <div className="bg-white p-6 rounded-lg shadow-md flex flex-col w-1/3">
               <div className="justify-end flex">
                 <button
                   onClick={toggleModalRegister}
